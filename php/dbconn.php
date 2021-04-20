400dcf0351e242d4db29d808e9430302763080a7
Lotke0 <1037730299@qq.com>
2021-04-20 23:09:26 +0800

    change mysql account

--- php/dbconn.php
+++ php/dbconn.php
@@ -3,11 +3,9 @@
 $dbms='mysql';             //数据库类型
 $host='localhost';         //数据库主机名
 $dbName='yike';            //数据库名
-$user='root';              //数据库连接用户名
-$pass='root@lotke';                  //密码
+$user='yike';              //数据库连接用户名
+$pass='kRfFmTcPJP4w8DYM';                  //密码
 $dsn="$dbms:host=$host;dbname=$dbName";   //pdo数据源
-
-
 $db = new PDO($dsn,$user,$pass);
 //关闭pdo自动提交
 $db->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
